68250490dd814eab12a29997f3db335969f20152
xuanyu123 <15534996948@163.com>
2020-01-08 21:01:24 +0800

    本地工程添加到git

---
+++ object1/js/time.js
@@ -0,0 +1,23 @@
+var timer=null;
+function displayClock(num){//num是传入的startClock中的动态值
+    if(num<10){
+        return "0"+num;
+    }
+    else{
+        return num;
+    }
+}
+//停止计时
+function stopClock(){
+    clearTimeout(timer);
+}
+//开始计时
+function startClock(){
+    var time =new Date();
+    var hours=displayClock(time.getHours())+":";
+    var minutes=displayClock(time.getMinutes())+":";
+    var seconds=displayClock(time.getSeconds());
+    //显示时间
+    show.innerHTML=hours+minutes+seconds;//在id为show的块区域显示
+    timer=setTimeout("startClock()",1000);//延时器
+}
